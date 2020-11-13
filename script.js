98c4ede02c3ee596d96cc3ca75874b604e234585
Adamh-dot <59826541+Adamh-dot@users.noreply.github.com>
2020-11-13 15:33:40 +0000

    Added computer cards and reveal at end of game

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ const hearts = ['AH','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH
 const diamonds = ['AD','2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD'];
 const clubs = ['AC','2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC'];
 const spades = ['AS','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS'];
-const players = 1;
+const players = 2;
 const pack = hearts.concat(diamonds, clubs, spades);
 const dealCardButton = document.getElementById('deal-card');
 const cardResult = document.getElementById('card1');
 //const $cardDiv = [$('card1'), $('card2')];
 let newPack = [];
-let card1 = '';
-let card2 = '';
-let hand = '';
+let playerCard1 = '';
+let playerCard2 = '';
+let compCard1 = '';
+let compCard2 = '';
+let playerHand = '';
 
 function getRandomCard() {
-    card1 = pack[(Math.floor(Math.random() * pack.length))];
-    newPack = pack.filter(val => val !== card1); //removes card1 from pack so it cannot be dealt again
-    card2 = newPack[(Math.floor(Math.random() * newPack.length))]; //gets 2nd card from reduced pack size
-    newPack = newPack.filter(val => val !== card2); //removes card2 from pack so it cannot be dealt again
+    playerCard1 = pack[(Math.floor(Math.random() * pack.length))];
+    newPack = pack.filter(val => val !== playerCard1); //removes card1 from pack so it cannot be dealt again
+    compCard1 = newPack[(Math.floor(Math.random() * newPack.length))]
+    newPack = newPack.filter(val => val !== compCard1);
+    playerCard2 = newPack[(Math.floor(Math.random() * newPack.length))]; //gets 2nd card from reduced pack size
+    newPack = newPack.filter(val => val !== playerCard2); //removes card2 from pack so it cannot be dealt again
+    compCard2 = newPack[(Math.floor(Math.random() * newPack.length))]; 
+    newPack = newPack.filter(val => val !== compCard2); 
     console.log(newPack);
-    hand = `${card1} ${card2}`;
-    console.log(hand);
-    const card1Url = `PNG/${card1}.png`;
-    const card2Url = `PNG/${card2}.png`;
+    playerHand = `${playerCard1} ${playerCard2}`;
+    compHand = `${compCard1} ${compCard2}`;
+    console.log(playerHand);
+    console.log(compHand);
+    const card1Url = `PNG/${playerCard1}.png`;
+    const card2Url = `PNG/${playerCard2}.png`;
     document.getElementById('card1Img').src = card1Url;
     document.getElementById('card2Img').src = card2Url;
     console.log(dealCardButton.value);
@@ -38,9 +46,9 @@ const dealFlop = () => {
         const flop1Url = `PNG/${flop1}.png`;
         const flop2Url = `PNG/${flop2}.png`;
         const flop3Url = `PNG/${flop3}.png`;
-        document.getElementById('flop1Img').setAttribute("style", "height: 200px; width: 140px;");
-        document.getElementById('flop2Img').setAttribute("style", "height: 200px; width: 140px;");
-        document.getElementById('flop3Img').setAttribute("style", "height: 200px; width: 140px;");
+        //document.getElementById('flop1Img').setAttribute("style", "height: 200px; width: 140px;");
+        //document.getElementById('flop2Img').setAttribute("style", "height: 200px; width: 140px;");
+        //document.getElementById('flop3Img').setAttribute("style", "height: 200px; width: 140px;");
         document.getElementById('flop1Img').src = flop1Url;
         document.getElementById('flop2Img').src = flop2Url;
         document.getElementById('flop3Img').src = flop3Url;
@@ -64,6 +72,13 @@ const dealRiver = () => {
     document.getElementById('riverImg').src = riverUrl;
 }
 
+const revealCompCards = () => {
+    const compCard1Url = `PNG/${compCard1}.png`;
+    const compCard2Url = `PNG/${compCard2}.png`;
+    document.getElementById('compCard1Img').src = compCard1Url;
+    document.getElementById('compCard2Img').src = compCard2Url;
+}
+
 dealCardButton.addEventListener('click', () => {
     if(dealCardButton.value === 'Deal Hand') {
         getRandomCard(pack);
@@ -77,14 +92,12 @@ dealCardButton.addEventListener('click', () => {
     } else if (dealCardButton.value === 'River!') {
         dealRiver();
         console.log(newPack);
-        dealCardButton.value = 'Game Over!';
+        dealCardButton.value = 'Reveal!';
+    } else if (dealCardButton.value === 'Reveal!') {
+        revealCompCards();
+        dealCardButton.value = 'New Game!';
+        dealCardButton.setAttribute("style", "background-color: red;")
+    } else if (dealCardButton.value === 'New Game!') {
+        location.reload();
     }
 })
-
-//render card hand in html
-
-//const renderCard = (getRandomCard) => {
-//    const cardImgSrc = `PNG/${card1}.png`;
-//    let cardContent = `<img src=${cardImgSrc} />`;
-//    $cardDiv.append(cardContent);
-//}
