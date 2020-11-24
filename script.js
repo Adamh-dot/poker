3e42675b2f864eb679e0f417daa946afce21bf26
Adamh-dot <59826541+Adamh-dot@users.noreply.github.com>
2020-11-24 17:17:22 +0000

    added javascript to find pairs, three of a kind, full house. experimental login age added

--- script.js
+++ script.js
@@ -16,12 +16,35 @@ let compCard2 = '';
 let playerHand = [];
 let compHand = [];
 let flop =[];
+let finalHandInfo = {
+    suits: {
+        hearts: 0,
+        diamonds: 0,
+        clubs: 0,
+        spades: 0
+    },
+    ranks: {
+        Aces: 0,
+        Twos: 0,
+        Threes: 0,
+        Fours: 0,
+        Fives: 0,
+        Sixes: 0,
+        Sevens: 0,
+        Eights: 0,
+        Nines: 0,
+        Tens: 0,
+        Jacks: 0,
+        Queens: 0,
+        Kings: 0
+    }
+};
 
 function getRandomCard() {
-    playerCard1 = pack[(Math.floor(Math.random() * pack.length))];
-    dealtCards.push(playerCard1);
+    playerCard1 = pack[(Math.floor(Math.random() * pack.length))]; //selects player 1's first card
+    dealtCards.push(playerCard1); //adds first card to 
     newPack = pack.filter(val => val !== playerCard1); //removes card1 from pack so it cannot be dealt again
-    compCard1 = newPack[(Math.floor(Math.random() * newPack.length))]
+    compCard1 = newPack[(Math.floor(Math.random() * newPack.length))] //selects computer's first card
     dealtCards.push(compCard1);
     newPack = newPack.filter(val => val !== compCard1);
     playerCard2 = newPack[(Math.floor(Math.random() * newPack.length))]; //gets 2nd card from reduced pack size
@@ -107,7 +130,8 @@ const countSuits = (suitArray) => {
         } return false;
 }
 
-const findFlush = (sevenCardHand) => { //feed in any seven card hand array (output from "compHand" or "playerHand").
+/*feed in any seven card hand array (output from "compHand" or "playerHand").*/
+const findFlush = (sevenCardHand) => { 
     console.log(sevenCardHand);
     const handSplit = sevenCardHand.join("").split("");
     console.log(handSplit);
@@ -130,6 +154,111 @@ const findFlush = (sevenCardHand) => { //feed in any seven card hand array (outp
     //}
 }
 
+/* Function to put all hand information into object to analyse for hands later on*/
+const populateHandInfo = (sevenCardHand) => {
+    const handSplit = sevenCardHand.join("").split("");
+    console.log(handSplit);
+    const handSuits = []; //need to try to get these into a global object so can run multiple hands
+    const handRanks = []; //need to try to get these into a global object so can run multiple hands
+    console.log(handRanks);
+    for (i=0; i<handSplit.length; i++){
+        if (i % 2) {
+            handSuits.push(handSplit[i]);
+        } else {
+            handRanks.push(handSplit[i])
+        }
+    };
+    handRanks.forEach(card => {
+        switch(card) {
+            case 'A':
+                finalHandInfo.ranks.Aces++;
+            break;
+            case '2':
+                finalHandInfo.ranks.Twos++;
+            break;
+            case '3':
+                finalHandInfo.ranks.Threes++;
+            break;
+            case '4':
+                finalHandInfo.ranks.Fours++;
+            break;
+            case '5':
+                finalHandInfo.ranks.Fives++;
+            break;
+            case '6':
+                finalHandInfo.ranks.Sixes++;
+            break;
+            case '7':
+                finalHandInfo.ranks.Sevens++;
+            break;
+            case '8':
+                finalHandInfo.ranks.Eights++;
+            break;
+            case '9':
+                finalHandInfo.ranks.Nines++;
+            break;
+            case 'T':
+                finalHandInfo.ranks.Tens++;
+            break;
+            case 'J':
+                finalHandInfo.ranks.Jacks++;
+            break;
+            case 'Q':
+                finalHandInfo.ranks.Queens++;
+            break;
+            case 'K':
+                finalHandInfo.ranks.Kings++;
+            break;
+        }
+    })
+    handSuits.forEach(card => {
+        switch(card) {
+            case 'H':
+                finalHandInfo.suits.hearts++;
+            break;
+            case 'D':
+                finalHandInfo.suits.diamonds++
+            break;
+            case 'S':
+                finalHandInfo.suits.spades++;
+            break;
+            case 'C':
+                finalHandInfo.suits.clubs++
+            break;
+        }
+    })
+}
+
+/* function checks if hand has a pair (or three of a kind) by looking at finalHandInfo.ranks object for a property value of 2 or 3 */
+const handHasPair = (finalHandInfo) => {
+    const pairs = [];
+    const threes = [];
+    for (const prop in finalHandInfo.ranks) {
+        if (finalHandInfo.ranks[prop] === 2) {
+            pairs.push(prop);
+        } else if (finalHandInfo.ranks[prop] === 3) {
+            threes.push(prop);
+        }
+    }
+    if(pairs.length === 1 && threes.length === 1) {
+        console.log(`You have a FULL HOUSE: ${threes[0]} over ${pairs[0]}!`); //@ADAM - need to protect for scenario of two lots of three of a kind or three of a kind and two pairs??
+        document.getElementById('handResult').innerHTML = `You have a FULL HOUSE: ${threes[0]} over ${pairs[0]}!`;
+        document.getElementById("handResult").style.visibility = 'visible';
+    } else if (threes.length === 1) {
+        console.log(`You have THREE ${threes[0]}!`); //@ADAM - need to protect for scenario of two lots of three of a kind or three of a kind and two pairs??
+        document.getElementById('handResult').innerHTML = `You have THREE ${threes[0]}!`;
+        document.getElementById("handResult").style.visibility = 'visible';
+    } else if (pairs.length === 1) {
+        console.log(`You have a PAIR of ${pairs[0]}`);
+        document.getElementById('handResult').innerHTML = `You have a PAIR of ${pairs[0]}!`;
+        document.getElementById("handResult").style.visibility = 'visible';
+    } else if (pairs.length === 2) {
+        console.log(`You have TWO PAIRS: ${pairs[0]} and ${pairs[1]}`);
+        document.getElementById('handResult').innerHTML = `You have TWO PAIRS: ${pairs[0]} and ${pairs[1]}`;
+        document.getElementById("handResult").style.visibility = 'visible';
+    } 
+}
+
 dealCardButton.addEventListener('click', () => {
         if(dealCardButton.value === 'Deal Hand') {
         getRandomCard(pack);
@@ -142,13 +271,15 @@ dealCardButton.addEventListener('click', () => {
         dealCardButton.value = 'River!';
     } else if (dealCardButton.value === 'River!') {
         dealRiver();
+        populateHandInfo(playerHand);
+        handHasPair(finalHandInfo);
         dealCardButton.value = 'Reveal!';
     } else if (dealCardButton.value === 'Reveal!') {
         revealCompCards();
         dealCardButton.value = 'New Game!';
         dealCardButton.setAttribute("style", "background-color: red;")
-        findFlush(playerHand);
-        findFlush(compHand);
+        //findFlush(playerHand);
+        //findFlush(compHand);
     } else if (dealCardButton.value === 'New Game!') {
         location.reload();
     }
